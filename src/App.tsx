80040399744b2fdb7b2100e9740bb5f690606d87
Servio Palacios, Ph.D. <servio@palacios.com>
2025-08-22 17:54:57 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,6 @@ import './App.css'
 import ConfIAbleARTDemo from "./ConfIAbleARTDemo";
 
 export default function Page() {
-  return <ConfIAbleARTDemo apiUrl={process.env.NEXT_PUBLIC_CONFIABLE_API} />;
+  return <ConfIAbleARTDemo />;
 }
 
