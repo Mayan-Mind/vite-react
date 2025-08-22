d3ea2ff48c25c6ce91321c265957ae4db67f6320
Servio Palacios, Ph.D. <servio@palacios.com>
2025-08-22 18:22:02 -0400

    Update ConfIAbleARTDemo.tsx

--- src/ConfIAbleARTDemo.tsx
+++ src/ConfIAbleARTDemo.tsx
@@ -1,5 +1,4 @@
-#import React, { useEffect, useMemo, useRef, useState } from "react";
-import React, { useEffect, useRef, useState } from "react";
+import{ useEffect, useRef, useState } from "react";
 import.meta.env.VITE_*;
 
 /**
